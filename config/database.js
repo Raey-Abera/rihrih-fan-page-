b717fa90447711b83976a427ac13dda074b12596
Raey Abera <raeyabera@Raeys-MacBook-Pro.local>
2019-03-31 15:53:52 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://rih:rih@cluster0-bjlct.mongodb.net/test?retryWrites=true', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'rih'
+};
