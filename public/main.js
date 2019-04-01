79fc3b3570a7012a9074fd0a7dde66a52e553670
Raey Abera <raeyabera@Raeys-MacBook-Pro.local>
2019-03-31 21:53:44 -0400

    first commit

--- public/main.js
+++ public/main.js
@@ -1,13 +1,13 @@
-var thumbUp = document.getElementsByClassName("fa-thumbs-up");
+var thumbUp = document.getElementsByClassName("far fa-heart");
 var trash = document.getElementsByClassName("fa-trash");
-var thumbDown = document.getElementsByClassName("far fa-heart");
+var heartBreak = document.getElementsByClassName("fas fa-heart-broken");
 
 
 Array.from(thumbUp).forEach(function(element) {
       element.addEventListener('click', function(){
         const name = this.parentNode.parentNode.childNodes[1].innerText
         const msg = this.parentNode.parentNode.childNodes[3].innerText
-        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[7].innerText)
         fetch('messages', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
@@ -26,6 +26,31 @@ Array.from(thumbUp).forEach(function(element) {
         })
       });
 });
+
+Array.from(heartBreak).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        const heartBreak = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        fetch('messages', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg,
+            'thumbUp':heartBreak
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+
 Array.from(trash).forEach(function(element) {
       element.addEventListener('click', function(){
         const name = this.parentNode.parentNode.childNodes[1].innerText
